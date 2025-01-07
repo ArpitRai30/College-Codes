306c4617344dacc209658d1ce16c99e2fd2df604
ArpitRai30 <arpitrai2004@outlook.com>
2025-01-07 11:14:56 +0530

    Initial commit

---
+++ Day1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main(){
+    // printf("Hello World!");
+    // printf("My name is Arpit Rai");
+    // printf("Today is my first day in C programming");
+
+
+    //   int a=10, b=20, c=30, sum=0, avg=0;
+    //     sum=a+b+c;
+    //     avg=sum/3;
+    //     printf("Average of three numbers is: %d" ,avg);
+
+
+    // int a=10, b=20;
+    //     printf("given number of a and b is %d %d \n", a, b);
+    //     a=a+b;
+    //     b=a-b;
+    //     a=a-b;
+    //     printf("swap of number a and b is %d %d \n", a, b);
+
+
+    // float pi=3.14, rad=3, area=0, per=0;
+    // area=pi*rad*rad;
+    // per=2*pi*rad;
+    // printf("area and per of circle is %f %f", area, per);
+
+return 0;
+} 

c311924d7058a3c2538561a1dc5849657ded67f3
ArpitRai30 <arpitrai2004@outlook.com>
2025-01-21 14:47:18 +0530

    python

---
+++ Python/day1.py
@@ -0,0 +1,60 @@
+# Hello
+
+# print("Hello World")
+
+
+# program to find the sum of two numbers
+
+# a = int(input("enter first number: "))
+# b = int(input("enter second number: "))
+# s = a + b
+# print("sum of 2 numbers is: ", s)
+
+
+
+# average of three number
+
+# a = int(input("enter 1st number: "))
+# b = int(input("enter 2nd number: "))
+# c = int(input("enter 3rd number: "))
+# s = a + b + c
+# avg = s / 3
+# print("average of three numbers is: ", avg)
+
+
+
+
+# percentage
+
+# a = int(input("marks of subject 1: "))
+# b = int(input("marks of subject 2: "))
+# c = int(input("marks of subject 3: "))
+# d = int(input("marks of subject 4: "))
+# e = int(input("marks of subject 5: "))
+# s = a + b + c + d + e 
+# per = (s / 500) * 100
+# print("Sum is ", s, "\nPercentage is ", per,"%")
+
+
+
+
+# area and circumference of circle
+
+# import math
+# r = float(input("Enter radius of circle: "))
+# ar = math. pi * r * r
+# c =  2 * math.pi * r
+# print(f"Area of circle is {ar}\nCircumference is {c}")
+
+
+
+
+# area of scalene triangle 
+
+# import math
+# a = float(input("Enter side 1: "))
+# b = float(input("Enter side 2: "))
+# c = float(input("Enter side 3: "))
+# s = (a + b + c) / 2
+# ar = math.sqrt(s * (s-a) * (s-b) * (s-c))
+# print(f"Area is {ar:.4f}")
---
+++ Python/day2.py
@@ -0,0 +1,91 @@
+# convert uppercase & lowercase
+
+# text = str(input("Enter text: "))
+# print(text.upper())
+# print(text.lower())
+
+
+
+
+# number of occurence
+
+# text = "banana"
+# print(text.count("a"))
+
+
+
+
+# check starting and ending
+
+# text = "Hello, Pyhton!"
+# print(text.startswith("Hello"))
+# print(text.endswith("Hello"))
+
+
+
+
+# replace space with hyphen
+
+# text = "Python is amazing"
+# print(text.replace(" ", "-"))
+
+
+
+
+# extracting initials
+
+# def get_initials(fullName):
+#     names = fullName.split()
+#     initials = "".join([name[0].upper() for name in names])
+#     return initials
+
+# name = str(input("Enter full name: "))
+# print(get_initials(name))
+
+
+
+
+# count vowels
+
+# def count(text):
+#     vowels = "aeiouAEIOU"
+#     return sum(1 for char in text if char in vowels)
+
+# sentence = str(input("Enter sentence: "))
+# print(count(sentence))
+
+
+
+
+# reverse words
+
+# def reverse(text):
+#     words = text.split()
+#     reversed_sent = " ".join(reversed(words))
+#     return reversed_sent
+
+# sentence = str(input("Enter sentence: "))
+# print(reverse(sentence))
+
+
+
+
+# check palindrome
+
+# def palindrome(text):
+#     text = text.replace(" ", "").lower()
+#     return text == text[::-1]
+
+# word = str(input("Enter word: "))
+# if palindrome(word) == True:
+#     print("Word is palindrome")
+# else:
+#     print("Word is not a palindrome")
+
+
+
+
+# delete consonants
+
+
+

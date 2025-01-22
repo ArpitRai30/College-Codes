908b41a7caf06541c3e3c03a8e05fecf274cb0f9
ArpitRai30 <arpitrai2004@outlook.com>
2025-01-22 11:51:32 +0530

    day 2 exercises

--- Python/day2.py
+++ Python/day2.py
@@ -70,22 +70,122 @@
 
 
 
-# check palindrome
+# count different characters
+
+# text = str(input("Enter text: "))
+# a = str(input("Enter character to count: "))
+# print(text.count(a))
+
+
+
+
+# count number of words
+
+# text = str(input("Enter sentence: "))
+# words = len(text.split())
+# print(words)
+
+
+
+
+# sort characters in descending order
+
+# text = str(input("Enter string: "))
+# print(sorted(text)[::-1])
+
+
+
+
+# sort in lexicographical order (Dictionary order)
+
+# s = str(input("Enter string: "))
+# print(sorted(s))
+
+
+
+
+# concatenation of two strings
+
+# s1 = str(input("Enter string 1: "))
+# s2 = str(input("Enter string 2: "))
+# print(s1 + "",s2)
+
+
+
+
+# length of string
+
+# s = str(input("Enter string: "))
+# print(len(s))
+
+
+
+
+# length without using function
+
+# s = str(input("Enter string: "))
+# count = 0
+# for char in s:
+#     count += 1
+
+# print(count)
+
+
+
+
+ # check palindrome
 
 # def palindrome(text):
 #     text = text.replace(" ", "").lower()
 #     return text == text[::-1]
 
-# word = str(input("Enter word: "))
-# if palindrome(word) == True:
-#     print("Word is palindrome")
+# s = str(input("Enter string: "))
+# if palindrome(s) == True:
+#     print(f"{s} is a palindrome")
 # else:
-#     print("Word is not a palindrome")
+#     print(f"{s} is not a palindrome")
+
+
+
+
+# print first name
+
+# def firstName(fullName):
+#     name = fullName.split()
+#     return name[0]
+
+# name = str(input("Enter full name: "))
+# print(firstName(name))
 
 
 
 
 # delete consonants
+
+# str = str(input("Enter string: "))
+# newString = ""
+# for char in str:
+#     if char in "aeiouAEIOU":
+#         newString += char
+
+# print(newString)
+
+
+
+
+# perfect number 
+
+# n = int(input("Enter a number: "))
+# sum = 0
+# for i in range(1, (n // 2) + 1):
+#     if(n % i == 0):
+#         sum += i
+
+# if(sum == n):
+#     print(f"{n} is a perfect number")
+# else:
+#     print(f"{n} is not a perfect number")
+
 
 
 
